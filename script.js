1fd160e4200bef80548dbaa7726007553c437bc0
Akhilesh-Yadav6283 <akhiyadav74085@gmail.com>
2023-12-31 00:24:39 +0530

    my first file

--- script.js
+++ script.js
@@ -1,173 +1,193 @@
+document.addEventListener("DOMContentLoaded", function(){
 let recipes = [
-    {
-        "name": "Veggie Delight",
-        "imageSrc": "https://source.unsplash.com/random?veggies",
-        "time": "30 min",
-        "type": "veg",
-        "isLiked": false,
-        "rating": 4.2
-    },
-    {
-        "name": "Chicken Grill",
-        "imageSrc": "https://source.unsplash.com/random?chicken",
-        "time": "45 min",
-        "type": "non-veg",
-        "isLiked": false,
-        "rating": 4.5
-    },
-    {
-        "name": "Cheese Pizza",
-        "imageSrc": "https://source.unsplash.com/random?pizza",
-        "time": "40 min",
-        "type": "veg",
-        "isLiked": false,
-        "rating": 4.1
-    },
-    {
-        "name": "Steak",
-        "imageSrc": "https://source.unsplash.com/random?steak",
-        "time": "60 min",
-        "type": "non-veg",
-        "isLiked": false,
-        "rating": 4.7
-    },
-    {
-        "name": "Grilled Salmon",
-        "imageSrc": "https://source.unsplash.com/random?salmon",
-        "time": "50 min",
-        "type": "non-veg",
-        "isLiked": false,
-        "rating": 4.6
-    },
-    {
-        "name": "Tomato Pasta",
-        "imageSrc": "https://source.unsplash.com/random?pasta",
-        "time": "35 min",
-        "type": "veg",
-        "isLiked": false,
-        "rating": 4.0
-    },
-    {
-        "name": "Vegan Salad",
-        "imageSrc": "https://source.unsplash.com/random?salad",
-        "time": "20 min",
-        "type": "veg",
-        "isLiked": false,
-        "rating": 3.9
-    },
-    {
-        "name": "Fried Chicken",
-        "imageSrc": "https://source.unsplash.com/random?friedChicken",
-        "time": "55 min",
-        "type": "non-veg",
-        "isLiked": false,
-        "rating": 4.3
-    },
-    {
-        "name": "Mushroom Risotto",
-        "imageSrc": "https://source.unsplash.com/random?risotto",
-        "time": "45 min",
-        "type": "veg",
-        "isLiked": false,
-        "rating": 4.5
-    },
-    {
-        "name": "Burger",
-        "imageSrc": "https://source.unsplash.com/random?burger",
-        "time": "30 min",
-        "type": "non-veg",
-        "isLiked": false,
-        "rating": 4.2
-    },
-    {
-        "name": "Paneer Tikka",
-        "imageSrc": "https://images.unsplash.com/photo-1630748663402-ef49332b3fc2?q=80&w=1481&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "time": "40 min",
-        "type": "veg",
-        "isLiked": false,
-        "rating": 4.4
-    },
-    {
-        "name": "BBQ Ribs",
-        "imageSrc": "https://source.unsplash.com/random?ribs",
-        "time": "70 min",
-        "type": "non-veg",
-        "isLiked": false,
-        "rating": 4.6
-    },
-    {
-        "name": "Caesar Salad",
-        "imageSrc": "https://source.unsplash.com/random?caesarSalad",
-        "time": "25 min",
-        "type": "veg",
-        "isLiked": false,
-        "rating": 3.8
-    },
-    {
-        "name": "Fish Tacos",
-        "imageSrc": "https://media.istockphoto.com/id/543088478/photo/vegetable-salad-with-salmon-and-papaya-salad.jpg?s=1024x1024&w=is&k=20&c=KlxL40H6MsQShEu1-S0bI1Qb-0ZrODe8rWvnelyod-A=",
-        "time": "35 min",
-        "type": "non-veg",
-        "isLiked": false,
-        "rating": 4.3
-    },
-    {
-        "name": "Chocolate Cake",
-        "imageSrc": "https://source.unsplash.com/random?chocolateCake",
-        "time": "90 min",
-        "type": "veg",
-        "isLiked": false,
-        "rating": 4.9
-    },
-    {
-        "name": "Caesar Salad",
-        "imageSrc": "https://source.unsplash.com/random?caesarSalad",
-        "time": "25 min",
-        "type": "veg",
-        "isLiked": false,
-        "rating": 3.8
-    }
+  {
+    name: "Veggie Delight",
+    imageSrc: "https://source.unsplash.com/random?veggies",
+    time: "30 min",
+    type: "veg",
+    isLiked: false,
+    rating: 4.2,
+  },
+  {
+    name: "Chicken Grill",
+    imageSrc: "https://source.unsplash.com/random?chicken",
+    time: "45 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.5,
+  },
+  {
+    name: "Cheese Pizza",
+    imageSrc: "https://source.unsplash.com/random?pizza",
+    time: "40 min",
+    type: "veg",
+    isLiked: false,
+    rating: 4.1,
+  },
+  {
+    name: "Steak",
+    imageSrc: "https://source.unsplash.com/random?steak",
+    time: "60 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.7,
+  },
+  {
+    name: "Grilled Salmon",
+    imageSrc: "https://source.unsplash.com/random?salmon",
+    time: "50 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.6,
+  },
+  {
+    name: "Tomato Pasta",
+    imageSrc: "https://source.unsplash.com/random?pasta",
+    time: "35 min",
+    type: "veg",
+    isLiked: false,
+    rating: 4.0,
+  },
+  {
+    name: "Vegan Salad",
+    imageSrc: "https://source.unsplash.com/random?salad",
+    time: "20 min",
+    type: "veg",
+    isLiked: false,
+    rating: 3.9,
+  },
+  {
+    name: "Fried Chicken",
+    imageSrc: "https://source.unsplash.com/random?friedChicken",
+    time: "55 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.3,
+  },
+  {
+    name: "Mushroom Risotto",
+    imageSrc: "https://source.unsplash.com/random?risotto",
+    time: "45 min",
+    type: "veg",
+    isLiked: false,
+    rating: 4.5,
+  },
+  {
+    name: "Burger",
+    imageSrc: "https://source.unsplash.com/random?burger",
+    time: "30 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.2,
+  },
+  {
+    name: "Paneer Tikka",
+    imageSrc: "https://source.unsplash.com/random?paneerTikka",
+    time: "40 min",
+    type: "veg",
+    isLiked: false,
+    rating: 4.4,
+  },
+  {
+    name: "BBQ Ribs",
+    imageSrc: "https://source.unsplash.com/random?ribs",
+    time: "70 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.6,
+  },
+  {
+    name: "Caesar Salad",
+    imageSrc: "https://source.unsplash.com/random?caesarSalad",
+    time: "25 min",
+    type: "veg",
+    isLiked: false,
+    rating: 3.8,
+  },
+  {
+    name: "Fish Tacos",
+    imageSrc: "https://source.unsplash.com/random?fishTacos",
+    time: "35 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.3,
+  },
+  {
+    name: "Chocolate Cake",
+    imageSrc: "https://source.unsplash.com/random?chocolateCake",
+    time: "90 min",
+    type: "veg",
+    isLiked: false,
+    rating: 4.9,
+  },
+  {
+    name: "Grilled Salmon",
+    imageSrc: "https://source.unsplash.com/random?salmon",
+    time: "50 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.6,
+  },
 ];
 let filteredRecipes = [...recipes];
 let searchBar = document.getElementById("searchBar");
+document
+  .getElementById("showAll")
+  .addEventListener("click", () => filteredRecipesfn());
+document
+  .getElementById("showveg")
+  .addEventListener("click", () => filteredRecipesfn("veg"));
+document
+  .getElementById("shownonveg")
+  .addEventListener("click", () => filteredRecipesfn("non-veg"));
 
-document.getElementById("showAll").addEventListener("click", ()=>filteredRecipesfn());
-document.getElementById("showveg").addEventListener("click", ()=>filteredRecipesfn());
-document.getElementById("shownonveg").addEventListener("click", ()=>filteredRecipesfn());
-
-
-searchBar.addEventListener("input", (e)=>{
-     let searchText = e.target.value.toLowerCase();
-    filteredRecipes = recipes.filter( (recipe) =>
-     recipe.name.toLowerCase().includes(searchText)
-     );
-     DisplayRecipes(filteredRecipes);
+searchBar.addEventListener("input", (e) => {
+  let searchText = e.target.value.toLowerCase();
+  filteredRecipes = recipes.filter((recipe) =>
+    recipe.name.toLowerCase().includes(searchText)
+  );
+  DisplayRecipes(filteredRecipes);
 });
 
 let RecipeContainer = document.getElementById("recipeContainer");
 
-function DisplayRecipes(RecipesToDisplay){
-    RecipeContainer.innerHTML="";
-    RecipesToDisplay.forEach((recipe) => {
-        let card = document.createElement("div");
-        card.className="recipe-card";
-        card.innerHTML=`
-        <img src= "${recipes.imageSrc}"  height="200px" >
-         <h3>${recipes.name}</h3>  
-         <p>${recipes.type}</p>
-         <p>${recipes.rating}</p>
+function DisplayRecipes(RecipesToDisplay) {
+  RecipeContainer.innerHTML = "";
+  RecipesToDisplay.forEach((recipe) => {
+    let card = document.createElement("div");
+    card.className = "recipe-card";
+    card.innerHTML = `
+        <img src= "${recipe.imageSrc}"  height="200px" >
+         <h3>${recipe.name}</h3>  
+         <p>${recipe.type}</p>
+         <p>${recipe.rating}</p>
          `;
-        RecipeContainer.appendChild(card); 
-    });
+    RecipeContainer.appendChild(card);
+  });
 }
 DisplayRecipes(recipes);
 
-function filteredRecipesfn(type="all"){
-    if(type=="all"){
-        DisplayRecipes(recipes);
-    }
-    else{
-        filteredRecipes = recipes.filter( (recipe) => recipe.type==type);
-        DisplayRecipes(filteredRecipes);
-    }
+function filteredRecipesfn(type = "all") {
+  if (type == "all") {
+    DisplayRecipes(recipes);
+  } else {
+    filteredRecipes = recipes.filter((recipe) => recipe.type == type);
+    DisplayRecipes(filteredRecipes);
+  }
 }
+ document.querySelectorAll('input[name="rating"]').forEach( (radio)=>{
+    radio.addEventListener("change", filterByRating);
+ });
+
+function filterByRating(){
+    let ratingAbove = document.getElementById("ratingAbove").checked;
+    let ratingBelow = document.getElementById("ratingBelow").checked;
+
+    filteredRecipes = recipes.filter( (recipe) => {
+        if(ratingAbove && recipe.rating > 4.0) return true;
+        if(ratingBelow && recipe.rating < 4.0 ) return true;
+         return false;
+    });
+    DisplayRecipes(filteredRecipes);
+}
+});
